4c29f4462721c06ff77715881186da2b29375986
sapnakavan <sapnakavan52@gmail.com>
2022-07-10 15:20:01 +0100

    week5 java practice

---
+++ Test/src/codebustertesting/JavaPractice.java
@@ -0,0 +1,18 @@
+package codebustertesting;
+
+public class JavaPractice { //class JavaPractice
+    public static void main(String args[]) { //Main Method
+
+          //Local Variables
+        int a = 20;// declare int is datatype and a is variable name which store value 20
+        int b = 10;
+
+         System.out.println("Addition of a and b is =" +(a+b));//Printing statement which print addition
+         System.out.println("Subtraction of a and b is =" +(a-b));//Printing statement which print Subtraction
+         System.out.println("Multiplication of a and b is =" +(a*b));//Printing statement which print Multiplication
+         System.out.println("Division of a and b is ="+(a/b));//Printing statement which print Division
+
+        System.out.println("Answer is =" +(a+b) +" Subtraction is =" +(a-b) +" Multiplication is =" +(a*b) +" Division is =" +(a/b));
+
+    }
+}
---
+++ Test/src/primitive_datatypes/FloatingPoints.java
@@ -0,0 +1,11 @@
+package primitive_datatypes;
+
+public class FloatingPoints {
+    public static void  main(String[] args){
+        float a = 1.205f;
+        double b = 1.250d;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
