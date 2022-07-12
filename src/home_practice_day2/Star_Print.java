a42722a646414aa684ebd6081a0c4dcdf96099fc
sapnakavan <sapnakavan52@gmail.com>
2022-07-12 21:44:21 +0100

    Initial Commit:Basic structure and simple shape classes. Tests don't pass.

---
+++ src/home_practice_day2/Star_Print.java
@@ -0,0 +1,16 @@
+package home_practice_day2;
+
+public class Star_Print {
+    public static void main(String []args){
+        System.out.println("        *          ");
+        System.out.println("       * *         ");
+        System.out.println(" *****     *****        ");
+        System.out.println("  *           *         ");
+        System.out.println("     *  *  *         ");
+        System.out.println("  *  *   *   *         ");
+        System.out.println("  *          *           ");
+
+
+
+    }
+}
